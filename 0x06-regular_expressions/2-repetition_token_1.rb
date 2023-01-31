26f5c60bde1f055c08e6352b3acab2d32f5ed427
Provoski <provo.iyen27@gmail.com>
2023-01-31 06:19:17 -0800

    task 0 to 2 added on regular expression tasks

---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# Creates a Ruby script that accepts
+# one argument and pass it to a regular expression matching method
+# The word to match is "School"
+#
+puts ARGV[0].scan(/hb?t*n/).join
