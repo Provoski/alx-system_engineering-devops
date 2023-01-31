20707d4ad25d740cdae4a9063773f1a7f0d50c78
Provoski <provo.iyen27@gmail.com>
2023-01-31 07:29:52 -0800

    task 6 correction on regular expression task

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -2,4 +2,4 @@
 # Creates a Ruby script that accepts
 # one argument and pass it to a regular expression matching method
 #
-puts ARGV[0].scan(/^[0-9]{10}/).join
+puts ARGV[0].scan(/^[0-9]{10}$/).join
