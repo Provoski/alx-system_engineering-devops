bc91fe061ebce84349a2e494a84a6070a8dc53b2
Provoski <provo.iyen27@gmail.com>
2023-07-03 19:01:52 -0700

    .updated 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,17 @@
 # Script to install nginx using puppet
 
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
+}
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
 exec { 'configure':
   command  => 'sudo sed -i "/^http {/a         add_header X-Served-By  $(hostname);" /etc/nginx/nginx.conf',
   onlyif   => '! grep -q "add_header X-Served-By" /etc/nginx/nginx.conf',
