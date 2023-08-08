47c32a92bb9c9f3516fa5b47fb8622c2f4efcc8c
Provoski <provo.iyen27@gmail.com>
2023-08-08 16:52:09 -0700

    fix pycodestyel in task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -21,15 +21,19 @@ def recurse(subreddit, hot_list=[], after=None):
     full_url = url.format(subreddit, after)
     headers = {"User-Agent": user_agent}
     try:
-        response = requests.get(full_url, headers=headers, allow_redirects=False)
+        response = requests.get(
+                                full_url,
+                                headers=headers,
+                                allow_redirects=False
+                                )
         if response.status_code == 200:
-            data = response.json()
-            if "data" in data and "children" in data["data"]:
-                posts = data["data"]["children"]
+            dt = response.json()
+            if "data" in dt and "children" in dt["data"]:
+                posts = dt["data"]["children"]
                 for post in posts:
                     hot_list.append(post["data"]["title"])
-                if "after" in data["data"] and data["data"]["after"] is not None:
-                    recurse(subreddit, hot_list, data["data"]["after"])
+                if "after" in dt["data"] and dt["data"]["after"] is not None:
+                    recurse(subreddit, hot_list, dt["data"]["after"])
                 return hot_list
             else:
                 return None
